d43c76a9b8f32549b020d12bc4cfdc79e156be4f
jhkim14 <jhkim79797728@gmail.com>
2021-05-24 11:14:58 +0900

    1

--- bookTest-1/src/main/resources/static/booking/BookingPage/booking.js
+++ bookTest-1/src/main/resources/static/booking/BookingPage/booking.js
@@ -1,8 +1,16 @@
-function booking(){
-    const userId = document.getElementById('userId').value;
-    const userMenu= document.getElementById('userMenu').value;
-    const tableNum = document.getElementById('tableNum').value;
-    const startTime = document.getElementById('startTime').value;
-    alert("아이디 = "+userId+" 메뉴 = "+userMenu +" 테이블 = "+tableNum +" 시간 = "+startTime);
-    location.href = "main.html";
-}
+bookingBtn = document.getElementById("bookingBtn")
+bookingBtn.addEventListener('click', () => {
+	url = '/reservation/new_reservation?' +
+		'covers=' + document.getElementById('bookingCovers').value +
+		'&date=' + document.getElementById('bookingDate').value +
+		'&time=' + document.getElementById('bookingTime').value +
+		'&tno=' + document.getElementById('bookingTable').value +
+		'&name=' + document.getElementById('bookingUserName').value +
+		'&phone=' + document.getElementById('bookingPhoneNumber').value
+
+	fetch(url, {
+		method: 'POST'
+	})
+		.then(e => e.json())
+		.then(data => console.log(data))
+})
